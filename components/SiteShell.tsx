c13d789f283adc8be89567aa9ad368f58575bb35
kazzahrani <oxyginmusic@gmail.com>
2026-08-04 12:21:19 +0300

    Enable kinetic microscopy gallery for Prives design

--- components/SiteShell.tsx
+++ components/SiteShell.tsx
@@ -3,7 +3,7 @@ import type { CSSProperties } from "react";
 import BourdonDesign from "@/components/designs/BourdonDesign";
 import EditorialImageDesign from "@/components/designs/EditorialImageDesign";
 import EngelandDesignWithFigures from "@/components/designs/EngelandDesignWithFigures";
-import PhotoLabDesign from "@/components/designs/PhotoLabDesign";
+import KineticPhotoLabDesign from "@/components/designs/KineticPhotoLabDesign";
 import SignatureAcademicDesign from "@/components/designs/SignatureAcademicDesign";
 import SourcedBourdonResearchDesign from "@/components/designs/SourcedBourdonResearchDesign";
 import {
@@ -84,7 +84,7 @@ export default function SiteShell({ site, route, basePath, previewMode = false }
   }
 
   if (designVariant === "prives-photo-lab-v1") {
-    return <PhotoLabDesign site={site} route={route} basePath={resolvedBasePath} previewMode={previewMode} />;
+    return <KineticPhotoLabDesign site={site} route={route} basePath={resolvedBasePath} previewMode={previewMode} />;
   }
 
   if (designVariant === "zhang-transcription-v1") {
