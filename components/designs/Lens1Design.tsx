8b49e3482408fe84a40c54b6f88313124b2135fd
kazzahrani <oxyginmusic@gmail.com>
2026-08-11 23:36:18 +0300

    Isolate Lens_1 from legacy portrait routing

--- components/designs/Lens1Design.tsx
+++ components/designs/Lens1Design.tsx
@@ -16,9 +16,21 @@ type Props = {
  * Independent reusable portrait-led design based on the approved Susanne Lens concept.
  */
 export default function Lens1Design(props: Props) {
+  const site: LabSite = {
+    ...props.site,
+    design: {
+      ...props.site.design,
+      settings: {
+        ...props.site.design?.settings,
+        variant: "HDportrait_1",
+        templatePolicy: "HDportrait_1",
+      },
+    },
+  };
+
   if (props.route.section === "research" && props.route.projectSlug) {
-    return <HDPortraitProjectDesign {...props} />;
+    return <HDPortraitProjectDesign {...props} site={site} />;
   }
 
-  return <LensPortraitDesign {...props} />;
+  return <LensPortraitDesign {...props} site={site} />;
 }
