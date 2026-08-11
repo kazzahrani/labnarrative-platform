736b2997d307c299be24f1b375b594637541e3ac
kazzahrani <oxyginmusic@gmail.com>
2026-08-11 22:18:20 +0300

    Remove hero from HDportrait_1 project pages

--- components/designs/HDPortrait1Design.tsx
+++ components/designs/HDPortrait1Design.tsx
@@ -1,3 +1,20 @@
-export { default } from "@/components/designs/LensPortraitDesign";
+import HDPortraitProjectDesign from "@/components/designs/HDPortraitProjectDesign";
+import LensPortraitDesign from "@/components/designs/LensPortraitDesign";
+import type { LabSite, SiteRoute } from "@/lib/sites";
 
 export const HDPORTRAIT_1_VARIANT = "HDportrait_1";
+
+type Props = {
+  site: LabSite;
+  route: SiteRoute;
+  basePath: string;
+  previewMode?: boolean;
+};
+
+export default function HDPortrait1Design(props: Props) {
+  if (props.route.section === "research" && props.route.projectSlug) {
+    return <HDPortraitProjectDesign {...props} />;
+  }
+
+  return <LensPortraitDesign {...props} />;
+}
