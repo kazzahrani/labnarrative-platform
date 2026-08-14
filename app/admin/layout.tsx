c6f7a4f3c0c376e784da2534ee38147998bdd907
kazzahrani <oxyginmusic@gmail.com>
2026-08-15 02:51:09 +0300

    Mount Systems email batch launcher in admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import SystemsLinkedInBatchLauncher from "./SystemsLinkedInBatchLauncher";
+import SystemsEmailBatchLauncher from "./SystemsEmailBatchLauncher";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <SystemsEmailBatchLauncher />
       <SystemsLinkedInBatchLauncher />
     </>
   );
