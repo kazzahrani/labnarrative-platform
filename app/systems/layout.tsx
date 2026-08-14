063bfcda3f0eb097f1c2c6d3b4bef0371408bde4
kazzahrani <oxyginmusic@gmail.com>
2026-08-14 11:08:56 +0300

    Apply light theme as Systems default

--- app/systems/layout.tsx
+++ app/systems/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import ArabicDigitLocalizer from "./ArabicDigitLocalizer";
+import DefaultLightTheme from "./DefaultLightTheme";
 import "./typography.css";
 
 export default function SystemsLayout({ children }: { children: ReactNode }) {
   return (
     <div className="lnSystemsTypography">
+      <DefaultLightTheme />
       <ArabicDigitLocalizer />
       {children}
     </div>
