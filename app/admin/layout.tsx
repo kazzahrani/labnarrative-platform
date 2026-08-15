dec38c58c4658fe8850795ed063890a240031389
kazzahrani <oxyginmusic@gmail.com>
2026-08-15 03:00:58 +0300

    Mount Connected Systems prospect filter

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import SystemsLinkedInBatchLauncher from "./SystemsLinkedInBatchLauncher";
 import SystemsEmailBatchLauncher from "./SystemsEmailBatchLauncher";
+import SystemsConnectedFilterEnhancer from "./SystemsConnectedFilterEnhancer";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <SystemsConnectedFilterEnhancer />
       <SystemsEmailBatchLauncher />
       <SystemsLinkedInBatchLauncher />
     </>
