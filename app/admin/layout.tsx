4808f1f4e77d9b2cc0bea3eea169d3091267fcac
kazzahrani <oxyginmusic@gmail.com>
2026-08-15 16:19:19 +0300

    Mount simplified Systems outreach panel

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,6 +5,7 @@ import SystemsLinkedInBatchLauncher from "./SystemsLinkedInBatchLauncher";
 import SystemsEmailBatchLauncher from "./SystemsEmailBatchLauncher";
 import SystemsConnectedFilterEnhancer from "./SystemsConnectedFilterEnhancer";
 import SystemsContactSearchEnhancer from "./SystemsContactSearchEnhancer";
+import SystemsSimpleOutreachPanel from "./SystemsSimpleOutreachPanel";
 import SystemsThemeToggle from "./SystemsThemeToggle";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
@@ -15,6 +16,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
       <SystemsThemeToggle />
       <SystemsConnectedFilterEnhancer />
       <SystemsContactSearchEnhancer />
+      <SystemsSimpleOutreachPanel />
       <SystemsEmailBatchLauncher />
       <SystemsLinkedInBatchLauncher />
     </>
