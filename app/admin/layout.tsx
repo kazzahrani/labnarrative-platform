3e5506ff9fc4fff139a35ac58a9c0c70c2d9efc2
kazzahrani <oxyginmusic@gmail.com>
2026-08-15 13:32:09 +0300

    Mount Systems theme toggle

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,11 +3,13 @@ import SystemsLinkedInBatchLauncher from "./SystemsLinkedInBatchLauncher";
 import SystemsEmailBatchLauncher from "./SystemsEmailBatchLauncher";
 import SystemsConnectedFilterEnhancer from "./SystemsConnectedFilterEnhancer";
 import SystemsContactSearchEnhancer from "./SystemsContactSearchEnhancer";
+import SystemsThemeToggle from "./SystemsThemeToggle";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <SystemsThemeToggle />
       <SystemsConnectedFilterEnhancer />
       <SystemsContactSearchEnhancer />
       <SystemsEmailBatchLauncher />
