9f291e8cca8e9b183e890071473ef5872a092968
kazzahrani <oxyginmusic@gmail.com>
2026-08-15 13:04:28 +0300

    Mount Systems contact search enhancer

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,12 +2,14 @@ import type { ReactNode } from "react";
 import SystemsLinkedInBatchLauncher from "./SystemsLinkedInBatchLauncher";
 import SystemsEmailBatchLauncher from "./SystemsEmailBatchLauncher";
 import SystemsConnectedFilterEnhancer from "./SystemsConnectedFilterEnhancer";
+import SystemsContactSearchEnhancer from "./SystemsContactSearchEnhancer";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <SystemsConnectedFilterEnhancer />
+      <SystemsContactSearchEnhancer />
       <SystemsEmailBatchLauncher />
       <SystemsLinkedInBatchLauncher />
     </>
