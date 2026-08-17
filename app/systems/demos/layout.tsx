29339be436008e16eb438b8539ce0434b3a2c6c9
kazzahrani <oxyginmusic@gmail.com>
2026-08-17 15:02:06 +0300

    Protect Systems client demos from public marketing palette

---
+++ app/systems/demos/layout.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from "react";
+
+export default function SystemsDemosLayout({ children }: { children: ReactNode }) {
+  return <div className="lnSystemsDemoScope">{children}</div>;
+}
