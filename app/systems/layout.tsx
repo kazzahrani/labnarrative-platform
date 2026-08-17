003a6bf7c2825ee45a954c1d16fd40e15b3f07c1
kazzahrani <oxyginmusic@gmail.com>
2026-08-17 14:08:32 +0300

    Apply colorful theme to Systems public pages

--- app/systems/layout.tsx
+++ app/systems/layout.tsx
@@ -2,10 +2,11 @@ import type { ReactNode } from "react";
 import ArabicDigitLocalizer from "./ArabicDigitLocalizer";
 import DefaultLightTheme from "./DefaultLightTheme";
 import "./typography.css";
+import "./colorful-public.css";
 
 export default function SystemsLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="lnSystemsTypography">
+    <div className="lnSystemsTypography lnSystemsPublic">
       <DefaultLightTheme />
       <ArabicDigitLocalizer />
       {children}
