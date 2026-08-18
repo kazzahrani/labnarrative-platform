e1f4fb935ac4749882c62110ad6791147a235b6c
kazzahrani <oxyginmusic@gmail.com>
2026-08-19 01:07:44 +0300

    Allow Client V2 LinkedIn controller asset

--- app/api/experience-asset/route.ts
+++ app/api/experience-asset/route.ts
@@ -8,6 +8,7 @@ const ALLOWED = new Set([
   "/experience-v2.css",
   "/experience-v2-communications.css",
   "/experience-v2.js",
+  "/experience-v2-linkedin.js",
   "/experience.css",
   "/experience-outreach-controls.css",
   "/experience-activity.css",
