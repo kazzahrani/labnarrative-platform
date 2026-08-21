0780bc3291c6b4db03e19c0d21c53b8d4095c686
kazzahrani <oxyginmusic@gmail.com>
2026-08-21 13:09:49 +0300

    Load requirements-aware application pack

--- app/api/career/page/route.ts
+++ app/api/career/page/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
   const baseHtml = await readFile(filePath, "utf8");
   const html = baseHtml.replace(
     "</body>",
-    '<script src="/career-microsoft-ui.js"></script>\n</body>',
+    '<script src="/career-microsoft-ui.js"></script>\n<script src="/career-requirements-ui.js"></script>\n</body>',
   );
 
   return new Response(html, {
