5190d88a150b749e5e8afdf80713b3f562aa6b7b
kazzahrani <oxyginmusic@gmail.com>
2026-08-24 23:07:12 +0300

    Make Thrwa sync cadence migration portable

--- supabase/migrations/20260824200300_tighten_wealth_binance_sync_cadence.sql
+++ supabase/migrations/20260824200300_tighten_wealth_binance_sync_cadence.sql
@@ -1,1 +1,14 @@
-select cron.alter_job(43, schedule := '* * * * *');
+do $$
+declare
+  v_jobid bigint;
+begin
+  select jobid into v_jobid
+  from cron.job
+  where jobname = 'wealth-binance-background-sync'
+  limit 1;
+
+  if v_jobid is not null then
+    perform cron.alter_job(v_jobid, schedule := '* * * * *');
+  end if;
+end
+$$;
