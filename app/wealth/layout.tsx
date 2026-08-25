c271d7b87affe76542b03ca5976bb7c4b807c351
kazzahrani <oxyginmusic@gmail.com>
2026-08-25 10:49:04 +0300

    Use unified Thrwa language runtime

--- app/wealth/layout.tsx
+++ app/wealth/layout.tsx
@@ -5,14 +5,13 @@ import "./wealth-preferences.css";
 import WealthSaudiMarketAutoRefresh from "./WealthSaudiMarketAutoRefresh";
 import WealthNavEnhancer from "./WealthNavEnhancer";
 import WealthPricingIntegrity from "./WealthPricingIntegrity";
-import WealthPreferences from "./WealthPreferences";
-import WealthEnglishFinalizer from "./WealthEnglishFinalizer";
+import WealthPreferencesV2 from "./WealthPreferencesV2";
 import WealthThemeRuntime from "./WealthThemeRuntime";
 
 const preferenceBoot = `
 try {
-  var language = localStorage.getItem('tharwa:language') === 'en' ? 'en' : 'ar';
-  var theme = localStorage.getItem('tharwa:theme') === 'light' ? 'light' : 'dark';
+  var language = (localStorage.getItem('thrwa:language') || localStorage.getItem('tharwa:language')) === 'en' ? 'en' : 'ar';
+  var theme = (localStorage.getItem('thrwa:theme') || localStorage.getItem('tharwa:theme')) === 'light' ? 'light' : 'dark';
   document.documentElement.dataset.wealthLang = language;
   document.documentElement.dataset.wealthTheme = theme;
   document.documentElement.lang = language;
@@ -32,8 +31,7 @@ export default function WealthLayout({
       <script dangerouslySetInnerHTML={{ __html: preferenceBoot }} />
       <WealthSaudiMarketAutoRefresh />
       <WealthNavEnhancer />
-      <WealthPreferences />
-      <WealthEnglishFinalizer />
+      <WealthPreferencesV2 />
       <WealthThemeRuntime />
       <Suspense fallback={null}>
         <WealthPricingIntegrity />
