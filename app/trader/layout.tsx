70f2ff842ae0524e8524100e5b5bfe783f301937
kazzahrani <oxyginmusic@gmail.com>
2026-08-29 23:11:49 +0300

    Load larger Trader sidebar icons

--- app/trader/layout.tsx
+++ app/trader/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./brand-mark.css";
+import "./sidebar-icons.css";
 
 export default function TraderLayout({ children }: Readonly<{ children: ReactNode }>) {
   return <>{children}</>;
