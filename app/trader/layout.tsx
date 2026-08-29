098eae19bd653420d7971515336bb47301b2fcda
kazzahrani <oxyginmusic@gmail.com>
2026-08-29 10:36:51 +0300

    Add LabNarrative quarter-pie brand mark and favicon

---
+++ app/trader/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./brand-mark.css";
+
+export default function TraderLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return <>{children}</>;
+}
