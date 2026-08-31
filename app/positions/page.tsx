d3df0793486e76a7ab66701759b1e74fb170bfa9
kazzahrani <oxyginmusic@gmail.com>
2026-08-31 21:11:00 +0300

    Expose Core V2 exit-plan control in new Positions app

--- app/positions/page.tsx
+++ app/positions/page.tsx
@@ -1,5 +1,9 @@
 import HostRoute from "../trader-v2/HostRoute";
+import CoreV2ExitPlanControl from "../trader/CoreV2ExitPlanControl";
 
 export default function PositionsPage() {
-  return <HostRoute view="positions" />;
+  return <>
+    <HostRoute view="positions" />
+    <CoreV2ExitPlanControl />
+  </>;
 }
