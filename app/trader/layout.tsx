be40671478932305558d1b39b8bda96bbe530eb2
kazzahrani <oxyginmusic@gmail.com>
2026-09-17 20:11:14 +0300

    Expose Creator and Affiliate section in trading platform

--- app/trader/layout.tsx
+++ app/trader/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import "./brand-mark.css";
 import "./sidebar-icons.css";
+import TraderCreatorAffiliateLink from "./TraderCreatorAffiliateLink";
 
 export default function TraderLayout({ children }: Readonly<{ children: ReactNode }>) {
-  return <>{children}</>;
+  return <>{children}<TraderCreatorAffiliateLink /></>;
 }
